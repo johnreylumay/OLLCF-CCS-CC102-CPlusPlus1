bfbc10390f893d660a7434ba6df87edc65e8029e
John Rey Lumay <jrlumay22@gmail.com>
2024-10-15 10:13:02 +0800

    Add files via upload

---
+++ miterm_exam_lumay.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float sugarPrice , ricePrice , sardinesPrice , coffeePrice , MilkPrice;
+	float sugarQty , riceQty , sardinesQty;
+	float coffeePrice , MilkPrice , sugarQty , riceQty , sardinesQty;
+	float usdtoPhp, poundtoPhp, totalcostPhp, rate = 0;
+	
+	cout << "sugarPriceUSB: ";
+	cin >> sugarPrice;
+	cout << "ricePricePound: ";
+	cin >> ricePrice;
+	cout << "sardinesPricePound: ";
+	cin >> sardinesPrice;
+	cout << "coffeePriceUSD: ";
+	cin >> coffeePrice;
+	cout << "MilkPriceUSD: ";
+	cin >> MilkPrice;
+	cout << "sugarQty: ";
+	cin >> sugarQty;
+	cout >> "riceQty: ";
+	coin >> riceQty;
+	cout >> "sardinesQty: ";
+	cin >> sardinesQty;
+	
+	sugarPriceUSB = sugarQty * sugarcost;
+	ricePricePound = ricepoundQty * ricepoundcost;
+	sardinesPrice = sardinesQty * sardinescost;
+	coffeePrice =  coffeePriQty * coffeePricost;
+	MilkPriceUSD = MilkPriceQty * MilkPricecost;
+	
+	
+	
+	return 0;
+}
