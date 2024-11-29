2ca429311ec598064851fa16525f279e0089c593
John Rey Lumay <jrlumay22@gmail.com>
2024-11-29 15:43:37 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string SN, DP, CL, FN, PA;
+
+    cout << "School Name: ";
+    cin >> SN;
+    cout << "Department: ";
+    cin >> DP;
+    cout << "Course & Year Level: ";
+    cin >> CL;
+    cout << "Your Full Name: ";
+    cin >> FN;
+    cout << "Your Permanent Address: ";
+    cin >> PA;
+
+    return 0;
+}
